b2a6701200a457849a55d85f8c805d718a9ab9fc
Victor Avila <victor.avila@venturplex.com>
2020-01-25 09:37:27 -0700

    feat: add material and setup base styles

---
+++ libs/material/src/lib/material.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatButtonToggleModule
+} from '@angular/material';
+
+const MODULES = [
+  BrowserAnimationsModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatButtonToggleModule
+];
+
+@NgModule({
+  imports: MODULES,
+  exports: MODULES
+})
+export class MaterialModule {}
